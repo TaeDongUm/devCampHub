feb08529c5adfb2d34d5507f28e795f8879db467
etd93 <etd93@naver.com>
2025-10-04 06:54:54 +0900

    feat: 페이지 연결

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,14 @@ import StudentHome from "./pages/StudentHome";
 import CampDetail from "./pages/CampDetail";
 import DashBoardHome from "./pages/DashBoardHome";
 import Settings from "./pages/Settings";
+import Notice from "./pages/Notice";
+import Qna from "./pages/Qna";
+import Resources from "./pages/Resources";
+import Lounge from "./pages/Lounge";
+import StudyQuestions from "./pages/StudyQuestions";
+import LiveLecture from "./pages/LiveLecture";
+import Mogakco from "./pages/Mogakco";
+import MyAttendance from "./pages/MyAttendance";
 
 function RoutedHome() {
   const navigate = useNavigate();
@@ -31,6 +39,14 @@ export default function App() {
         <Route path="/camp/:id" element={<CampDetail />} />
         <Route path="/dashboard/home" element={<DashBoardHome />} />
         <Route path="/settings" element={<Settings />} />
+        <Route path="/camp/:campId/notice" element={<Notice />} />
+        <Route path="/camp/:campId/qna" element={<Qna />} />
+        <Route path="/camp/:campId/resources" element={<Resources />} />
+        <Route path="/camp/:campId/lounge" element={<Lounge />} />
+        <Route path="/camp/:campId/study-questions" element={<StudyQuestions />} />
+        <Route path="/camp/:campId/live" element={<LiveLecture />} />
+        <Route path="/camp/:campId/mogakco" element={<Mogakco />} />
+        <Route path="/camp/:campId/attendance" element={<MyAttendance />} />
       </Routes>
     </BrowserRouter>
   );
