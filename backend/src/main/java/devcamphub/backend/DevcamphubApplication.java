4b3e75aef2779f755c1cd421edd8b4ca2c9d9a32
etd93 <etd93@naver.com>
2025-10-09 12:44:13 +0900

    fix: repository 생성시 redis일지 jpa일지 확실히 정해주기

--- backend/src/main/java/devcamphub/backend/DevcamphubApplication.java
+++ backend/src/main/java/devcamphub/backend/DevcamphubApplication.java
@@ -2,11 +2,12 @@ package devcamphub.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "devcamphub.backend.repository")
 public class DevcamphubApplication {
 
 	public static void main(String[] args) {
